3ef595b30d4a51eb56e24c910cf665ea2be7a90d
Ana Gabriela <ana.sobrinho.703@ufrn.edu.br>
2022-02-17 20:20:08 -0300

    Add files via upload

---
+++ run.R
@@ -0,0 +1,20 @@
+## global.R ##
+
+# CARREGAMENTO DE PACOTES
+library(shiny)
+library(shinythemes)
+library(shinycssloaders)
+library(ggplot2)
+library(dplyr)
+library(tmap)
+#devtools::install_github("rpradosiqueira/brazilmaps"
+library(brazilmaps)
+library(viridisLite)
+
+port <- Sys.getenv('PORT')
+
+shiny::runApp(
+  appDir = getwd(),
+  host = '0.0.0.0',
+  port = as.numeric(port)
+)
